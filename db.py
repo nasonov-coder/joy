54bd596dc9c5af579fbb8b3d39d89b04df3c5208
dmitry <danasonov@yandex.ru>
2021-08-10 06:11:11 +0300

    query fixes

--- db.py
+++ db.py
@@ -1,12 +1,53 @@
 import logging
 import sys
-from couchbase.cluster import Cluster, PasswordAuthenticator, ClusterOptions
+from couchbase.cluster import Cluster, PasswordAuthenticator, ClusterOptions, QueryOptions
+from couchbase.n1ql import QueryResult
 
 logging.basicConfig(stream=sys.stderr, level=logging.FATAL)
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 # open cluster and authenticate as Cluster Admin
+
 # зайти в консоль - http://116.203.213.167:8091
 cluster = Cluster('couchbase://116.203.213.167',
                   ClusterOptions(PasswordAuthenticator('Administrator', 'wefbax-3wyKtu-cecbor')))
 bucket = cluster.bucket("joy")
 collection = bucket.default_collection()
+
+
+def upsert(type: str, id: str, doc: object):
+    collection.upsert(f'{type}@{id}', {'id': id, 'type': "song", 'doc': doc})
+
+
+def replace(type: str, id: str, doc: object):
+    collection.replace(f'{type}@{id}', {'id': id, 'type': "song", 'doc': doc})
+
+
+def remove(type: str, id: str):
+    collection.remove(f'{type}@{id}')
+
+
+def get(type: str, id: str):
+    collection.get(f'{type}@{id}')
+
+
+class Params:
+    dic = {}
+    num = 0
+
+    def __call__(self, obj: object):
+        self.num += 1
+        self.dic[f'p{self.num}'] = obj
+        return f'$p{self.num}'
+
+
+def query(sql: str, params: Params = None) -> QueryResult:
+    qo = None
+    if params is not None:
+        qo = QueryOptions(named_parameters=params.dic)
+    return cluster.query(sql, qo)
+
+
+def create_params() -> Params:
+    return Params()
+
--- main.py
+++ main.py
@@ -2,39 +2,50 @@
 
 import uvicorn
 from fastapi import FastAPI
-import jsonpickle
 # import couchbase.subdocument as SD
 
 
-from Types import Song, doc
-from db import cluster, collection
+from Types import Song
+from db import remove, upsert, get, create_params, query
 
 app = FastAPI()
 
 
 @app.get("/helloWorld")
-async def read_root():
+async def hello_world():
     return "HelloWorld"
 
 
 @app.get("/hello/{name}")
-async def read_root(name):
+async def hello(name):
     return f'Hello, {name}'
-
-
-@app.get("/song/list")
-async def list_songs() -> list[Song]:
-    return cluster.query('SELECT * FROM joy where type == "song"').rows()
 
 
 @app.put("/song/{id}")
 async def put_song(id: str, song: Song):
-    collection.upsert(f'song@{id}', {'id': id, 'type': "song", 'doc': song.dict()})
+    upsert('song', id, song.dict())
 
 
 @app.delete("/song/{id}")
-async def put_song(id: str):
-    return collection.remove(f'song@{id}')
+async def delete_song(id: str):
+    return remove('song', id)
+
+
+@app.get("/song/list")
+async def list_songs() -> list[Song]:
+    return query('SELECT * FROM joy WHERE type == "song"').rows()
+
+
+@app.get("/song/filter")
+async def list_songs_filter(author: str = "") -> list[Song]:
+    p = create_params()
+    return query(f'SELECT * FROM joy WHERE type == "song" AND `doc`.`author` LIKE {p(f"%{author}%")}', p).rows()
+
+
+# если поставить эту функцию выше, то она будет иметь приоитет над song/list или song/filter
+@app.get("/song/{id}")
+async def get_song(id: str):
+    return get('song', id)
 
 
 if __name__ == "__main__":
